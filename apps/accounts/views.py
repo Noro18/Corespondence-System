218a57f3bcad5877a1c772f1d7f8831b913d4134
Noro18 <zeroxg534@gmail.com>
2026-07-23 23:23:43 +0900

    Add ADMIN-only User CRUD (list, create, edit, deactivate)
    - UserListView with pagination (25 per page)
    - UserCreateView using UserCreationForm
    - UserUpdateView using UserChangeForm
    - UserDeleteView deactivates (is_active=False) instead of hard-delete
    - All views guarded with AdminMixin
    - Sidebar: 'Users' link for ADMIN only
    - Topbar 'Admin' link now ADMIN-only

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
-# Create your views here.
+from apps.common.mixins import AdminMixin
+
+from .models import CustomUser
+
+
+class UserListView(AdminMixin, LoginRequiredMixin, ListView):
+    model = CustomUser
+    template_name = "accounts/user_list.html"
+    context_object_name = "users"
+    paginate_by = 25
+    ordering = ["-is_active", "username"]
+
+
+class UserCreateView(AdminMixin, LoginRequiredMixin, CreateView):
+    model = CustomUser
+    form_class = UserCreationForm
+    template_name = "accounts/user_form.html"
+    extra_context = {"title": "Add User"}
+    success_url = reverse_lazy("accounts:user_list")
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        for field_name in form.fields:
+            form.fields[field_name].widget.attrs.update(
+                {"class": "w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"}
+            )
+        return form
+
+
+class UserUpdateView(AdminMixin, LoginRequiredMixin, UpdateView):
+    model = CustomUser
+    form_class = UserChangeForm
+    template_name = "accounts/user_form.html"
+    extra_context = {"title": "Edit User"}
+    success_url = reverse_lazy("accounts:user_list")
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        for field_name in form.fields:
+            form.fields[field_name].widget.attrs.update(
+                {"class": "w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"}
+            )
+        return form
+
+
+class UserDeleteView(AdminMixin, LoginRequiredMixin, DeleteView):
+    model = CustomUser
+    template_name = "accounts/user_confirm_delete.html"
+    success_url = reverse_lazy("accounts:user_list")
+
+    def form_valid(self, form):
+        self.object.is_active = False
+        self.object.save()
+        return super().form_valid(form)
