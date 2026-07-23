3fe8ef316ac1b389970750cc6da858477fdf6ff3
Noro18 <zeroxg534@gmail.com>
2026-07-23 10:00:00 +0900

    Add inbound letters views (List, Create, Detail, Delete)

--- apps/inbound_letters/views.py
+++ apps/inbound_letters/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, ListView
 
-# Create your views here.
+from apps.common.mixins import AdminMixin, SekretariaduMixin
+
+from .models import InboundLetter
+
+
+class InboundLetterListView(LoginRequiredMixin, ListView):
+    model = InboundLetter
+    template_name = "inbound_letters/letter_list.html"
+    context_object_name = "letters"
+    paginate_by = 25
+
+    def get_queryset(self):
+        qs = InboundLetter.objects.select_related("sender", "registered_by")
+        user = self.request.user
+        if user.role in [user.Role.ADMIN, user.Role.PREZIDENTE, user.Role.SEKRETARIADU]:
+            return qs
+        return qs.filter(assignments__assigned_to=user)
+
+
+class InboundLetterCreateView(SekretariaduMixin, LoginRequiredMixin, CreateView):
+    model = InboundLetter
+    template_name = "inbound_letters/letter_form.html"
+    fields = [
+        "title", "original_ref_no", "sender", "letter_date",
+        "pdf_file", "description", "notes",
+    ]
+    extra_context = {"title": "Register Inbound Letter"}
+    success_url = reverse_lazy("inbound_letters:list")
+
+    def form_valid(self, form):
+        form.instance.registered_by = self.request.user
+        return super().form_valid(form)
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields["letter_date"].widget.attrs["type"] = "date"
+        for field in form.fields.values():
+            field.widget.attrs.setdefault("class",
+                "w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+            )
+        return form
+
+
+class InboundLetterDetailView(LoginRequiredMixin, DetailView):
+    model = InboundLetter
+    template_name = "inbound_letters/letter_detail.html"
+    context_object_name = "letter"
+
+    def get_queryset(self):
+        qs = InboundLetter.objects.select_related("sender", "registered_by").prefetch_related("assignments__assigned_to")
+        user = self.request.user
+        if user.role in [user.Role.ADMIN, user.Role.PREZIDENTE, user.Role.SEKRETARIADU]:
+            return qs
+        return qs.filter(assignments__assigned_to=user)
+
+
+class InboundLetterDeleteView(AdminMixin, LoginRequiredMixin, DeleteView):
+    model = InboundLetter
+    template_name = "inbound_letters/letter_confirm_delete.html"
+    success_url = reverse_lazy("inbound_letters:list")
