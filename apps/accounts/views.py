08db057b223302bdb9448dfb61b1f45dd0a1807a
abindacarmo <abindacarmo08@gmail.com>
2026-07-27 00:07:20 +0900

    fix(accounts): fix misaligned Active label and checkbox in user edit form

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -9,6 +9,8 @@ from apps.common.mixins import AdminMixin
 from .models import CustomUser
 
 
+import django.forms
+
 class CustomUserCreationForm(BaseUserCreationForm):
     class Meta(BaseUserCreationForm.Meta):
         model = CustomUser
@@ -38,10 +40,15 @@ class UserCreateView(AdminMixin, LoginRequiredMixin, CreateView):
 
     def get_form(self, form_class=None):
         form = super().get_form(form_class)
-        for field_name in form.fields:
-            form.fields[field_name].widget.attrs.update(
-                {"class": "w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"}
-            )
+        for field_name, field in form.fields.items():
+            if isinstance(field.widget, django.forms.CheckboxInput):
+                field.widget.attrs.update(
+                    {"class": "h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"}
+                )
+            else:
+                field.widget.attrs.update(
+                    {"class": "w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"}
+                )
         return form
 
 
@@ -54,10 +61,15 @@ class UserUpdateView(AdminMixin, LoginRequiredMixin, UpdateView):
 
     def get_form(self, form_class=None):
         form = super().get_form(form_class)
-        for field_name in form.fields:
-            form.fields[field_name].widget.attrs.update(
-                {"class": "w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"}
-            )
+        for field_name, field in form.fields.items():
+            if isinstance(field.widget, django.forms.CheckboxInput):
+                field.widget.attrs.update(
+                    {"class": "h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"}
+                )
+            else:
+                field.widget.attrs.update(
+                    {"class": "w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"}
+                )
         return form
 
 
