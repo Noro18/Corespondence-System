7a4388ae21aa0e10b567dee0207643734b00762f
Noro18 <zeroxg534@gmail.com>
2026-08-15 09:00:00 +0900

    feat: rework letter categories (Audensia, Pedidu, Proposta)

--- apps/common/choices.py
+++ apps/common/choices.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class LetterCategory(models.TextChoices):
-    ASSUNTO = "ASU", "Asuntu"
-    INFORMACAO = "INF", "Informasaun"
     CONVITE = "KVT", "Konvite"
+    AUDENCIA = "AUD", "Audensia"
+    PEDIDU = "PED", "Pedidu (skala kiik)"
+    PROPOSTA = "PRP", "Proposta (skala boot)"
