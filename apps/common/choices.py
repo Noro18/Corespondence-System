2b0094e3fc2a0a8873073c94fed6ff9bb4d5a64c
Noro18 <zeroxg534@gmail.com>
2026-08-18 22:36:07 +0900

    translate tetum to englsih for choices

--- apps/common/choices.py
+++ apps/common/choices.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class LetterCategory(models.TextChoices):
-    CONVITE = "KVT", "Konvite"
-    AUDENCIA = "AUD", "Audensia"
-    PEDIDU = "PED", "Pedidu (skala kiik)"
-    PROPOSTA = "PRP", "Proposta (skala boot)"
+    CONVITE = "KVT", "Invitation"
+    AUDENCIA = "AUD", "Audience"
+    PEDIDU = "PED", "Request (small scale)"
+    PROPOSTA = "PRP", "Proposal (large scale)"
